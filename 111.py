849e7a979eef21f7b90d3dd31eff54ef7e95dff7
Ken_H <1228337123hrg@gmail.com>
2020-12-24 04:58:28 +0800

    11155

---
+++ 111.py
@@ -0,0 +1,12 @@
+with open('F:\Git_Project\music/music_detail','r',encoding='utf-8') as f:
+    j = 0
+    js_list =[]
+    for js in f:
+        # js = json.loads(js)
+        # for v in js:
+        #     js[v] = str(js[v])
+        # # print(js)
+        # js_list.append(js)
+
+        j = j +1
+    print(j)
--- duqu.py
+++ duqu.py
@@ -15,18 +15,19 @@ with open('F:\Git_Project\music/music_detail','r',encoding='utf-8') as f:
         # print(js)
         js_list.append(js)
 
-        # j = j +1
-        # if j==5000:
-        #     # print(i)
-        #     f.close()
-        #     break
-
+        j = j +1
+        print(j)
+        if j==489860:
+            # print(i)
+            f.close()
+            break
+print('ok')
 result = pd.DataFrame()
 for js in js_list:
     df = pd.json_normalize(js)
     df = df[['singer_name', 'song_name', 'subtitle', 'album_name', 'singer_id', 'singer_mid', 'song_time_public', 'song_type', 'language', 'song_id', 'song_mid', 'song_url']]
     result = result.append(df)
-
+print('ok2')
 result.to_sql(name='music', con=engine, if_exists='append',index=False) #,dtype=dtypedict)  # 写入
 
 
